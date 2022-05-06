9e970ec2ff35d56acf94d46816139167725ad627
Dicky-27 <buwonodirga@gmail.com>
2022-05-06 12:32:39 +0700

    feat(LoginActivity,-SignUpActivity): Login and SignUp to firebase auth

---
+++ app/src/main/java/com/example/care/LoginActivity.kt
@@ -0,0 +1,88 @@
+package com.example.care
+
+import android.app.ProgressDialog
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
+import android.widget.Toast
+import androidx.appcompat.app.ActionBar
+import com.example.care.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityLoginBinding
+    private lateinit var actionBar: ActionBar
+    private lateinit var progressDialog: ProgressDialog
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    private var email = ""
+    private  var password = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        actionBar = supportActionBar!!
+        actionBar.title = "Login"
+
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Please wait")
+        progressDialog.setMessage("Logging In...")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        checkUser()
+
+        binding.tvAccount.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
+
+        binding.btnLogin.setOnClickListener {
+            validateData()
+        }
+
+    }
+
+    private fun validateData() {
+        email = binding.edEmail.text.toString().trim()
+        password = binding.edPassword.text.toString().trim()
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            binding.emailTil.error = "Invalid email format"
+        } else if (TextUtils.isEmpty(password)) {
+            binding.passwordTil.error = "Please enter password"
+        } else {
+            firebaseLogin()
+        }
+    }
+
+    private fun firebaseLogin() {
+        progressDialog.show()
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+                progressDialog.dismiss()
+                val firebaseUser = firebaseAuth.currentUser
+                val email = firebaseUser!!.email
+                Toast.makeText(this, "loggedIn as $email", Toast.LENGTH_SHORT).show()
+
+                startActivity(Intent(this, HomeTabBarActivity::class.java))
+                finish()
+            }
+            .addOnFailureListener { e->
+                progressDialog.dismiss()
+                Toast.makeText(this, "login  failed due to ${e.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun checkUser() {
+        val firebaseUser = firebaseAuth.currentUser
+        if (firebaseUser != null) {
+            startActivity(Intent(this, HomeTabBarActivity::class.java))
+            finish()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/care/fragment/ProfileFragment.kt
@@ -0,0 +1,51 @@
+package com.example.care.fragment
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.ActionBar
+import com.example.care.LoginActivity
+import com.example.care.R
+import com.example.care.databinding.FragmentProfileBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class ProfileFragment : Fragment() {
+
+    private lateinit var binding: FragmentProfileBinding
+    private lateinit var actionBar: ActionBar
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = FragmentProfileBinding.inflate(layoutInflater)
+        firebaseAuth = FirebaseAuth.getInstance()
+        checkUser()
+
+        binding.btnLogout.setOnClickListener {
+            firebaseAuth.signOut()
+            checkUser()
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return binding.root
+    }
+
+    private fun checkUser() {
+        val firebaseUser = firebaseAuth.currentUser
+        if (firebaseUser != null) {
+            val email = firebaseUser.email
+            binding.tvEmail.text = email
+        } else {
+            val intent = Intent (getActivity(), LoginActivity::class.java)
+            getActivity()?.startActivity(intent)
+        }
+    }
+}
